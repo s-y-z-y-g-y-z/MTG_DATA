cfa3e516ee78f10fdd16a60ccd0b4971e37c0e6e
Zachary Lopez <ztl25@drexel.edu>
2017-11-26 14:28:17 -0800

    Added swing class for Swing GUI

---
+++ my-app/src/main/java/com/mtg/app/Swing.java
@@ -0,0 +1,13 @@
+package com.mtg.app;
+
+import javax.swing.*;
+
+
+public class Swing {
+
+    JMenuBar menuBar;
+    JMenu menu, submenu;
+    JMenuItem menuItem;
+    JRadioButtonMenuItem rbMenuItem;
+    JCheckBoxMenuItem cbMenuItem;
+}
